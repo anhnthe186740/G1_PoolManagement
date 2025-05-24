1a91fa7f5384db83d61a98b422f67a7492296938
anhnthe186740 <nguyentheanh23022004@gmail.com>
2025-05-25 00:36:03 +0700

    done login register

---
+++ src/main/java/com/example/poolmanagement/config/CustomSuccessHandler.java
@@ -0,0 +1,16 @@
+package com.example.poolmanagement.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomSuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+            Authentication authentication) throws IOException {
+        // Chuyển hướng đến /api/auth/profile sau khi đăng nhập thành công
+        response.sendRedirect("/api/auth/profile");
+    }
+}
---
+++ src/main/java/com/example/poolmanagement/controller/HomeController.java
@@ -0,0 +1,61 @@
+package com.example.poolmanagement.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.poolmanagement.entity.User;
+import com.example.poolmanagement.service.AuthService;
+
+import io.jsonwebtoken.Claims;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    private final AuthService authService;
+
+    @GetMapping("/login")
+    public String getMethodName(Model model) {
+        model.addAttribute("user", new User());
+        return "auth/login";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("user", new User());
+        return "auth/register";
+    }
+
+    // @PostMapping("/login")
+    // public String login(@ModelAttribute("user") User user, Model model) {
+    // User loggedInUser = authService.login(user);
+    // if (loggedInUser == null) {
+    // model.addAttribute("error", "Invalid username or password");
+    // return "auth/login";
+    // }
+    // Claims claim = jwtUtil.getClaimsFromToken(loggedInUser.getToken());
+    // User newUser = new User();
+    // newUser.setUsername(claim.getSubject());
+    // newUser.setEmail(claim.get("email", String.class));
+    // newUser.setRole(claim.get("role", String.class));
+    // System.out.println(claim.get("role", String.class));
+    // System.out.println(claim.get("email", String.class));
+    // System.out.println(claim.getSubject());
+    // model.addAttribute("user", newUser);
+    // return "auth/profile";
+    // }
+
+    @GetMapping("/logout")
+    public String getMethodName2() {
+        return "/";
+    }
+
+}
