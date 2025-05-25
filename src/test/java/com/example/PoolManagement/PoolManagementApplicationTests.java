d679314d704bb8e0a52a336bc3b275a78e5c91c9
phamquocbinh13 <binhpqhe170203@fpt.edu.vn>
2025-05-25 18:34:06 +0700

    25/05: Update to merge without error with team MySQL/MSSQ Profile - Currentlly: Profile MySQL for team usage.

--- src/test/java/com/example/PoolManagement/PoolManagementApplicationTests.java
+++ src/test/java/com/example/PoolManagement/PoolManagementApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("mssql") // || Only for branch/binh MSSQL
+// @ActiveProfiles("mssql") // || Only for branch/binh MSSQL
 @SpringBootTest
 class PoolManagementApplicationTests {
 
